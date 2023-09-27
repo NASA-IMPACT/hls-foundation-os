9968269915db8402bf4a6d0549df9df57d489e5a
Carlos Gomes <carlos.gomes@ibm.com>
2023-09-27 12:10:38 +0200

    switch to rioxarray

--- configs/burn_scars.py
+++ configs/burn_scars.py
@@ -56,7 +56,7 @@ save_path = work_dir
 
 save_path = work_dir
 train_pipeline = [
-    dict(type='LoadGeospatialImageFromFile', to_float32=image_to_float32, channels_last=True),
+    dict(type='LoadGeospatialImageFromFile', to_float32=image_to_float32),
     dict(type='LoadGeospatialAnnotations', reduce_zero_label=False),
     dict(type='BandsExtract', bands=bands),
     dict(type='RandomFlip', prob=0.5),
@@ -74,7 +74,7 @@ train_pipeline = [
     dict(type='Collect', keys=['img', 'gt_semantic_seg'])
 ]
 test_pipeline = [
-    dict(type='LoadGeospatialImageFromFile', to_float32=image_to_float32, channels_last=True),
+    dict(type='LoadGeospatialImageFromFile', to_float32=image_to_float32),
     dict(type='BandsExtract', bands=bands),
     dict(type='ToTensor', keys=['img']),
     # to channels first
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     install_requires=
     [
         "mmsegmentation @ git+https://github.com/open-mmlab/mmsegmentation.git@186572a3ce64ac9b6b37e66d58c76515000c3280",
-        "tifffile",
+        "rioxarray",
         "einops",
         "timm==0.4.12",
         "tensorboard",
